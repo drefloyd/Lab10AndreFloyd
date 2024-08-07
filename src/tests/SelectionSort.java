5a9f778dc7e82271535bcb28c8a1a0d6b09bac37
andre floyd <64279881+drefloyd@users.noreply.github.com>
2024-08-06 20:37:41 -0400

    updated code

--- src/tests/SelectionSort.java
+++ src/tests/SelectionSort.java
@@ -7,15 +7,20 @@ public class SelectionSort {
   }
 
     public int[] basicSelectionSort(int[] x){
-        for(int i =0; i < x.length; ++i) {
-        	for(int j=i+1; j <x.length; ++j) {
-        		if(x[i] > x[j]) {
-        			temp = x[i];
-        			x[i] = x[j];
-        			temp = x[j];
-        		}
-        	}
+        for (int i = 0; i < x.length - 1; i++) {
+            int lowestIndex = i;
+
+            for (int j = i + 1; j < x.length; j++) {
+                if (x[j] < x[lowestIndex]) {
+                    lowestIndex = j;
+                }
+            }
+            if (lowestIndex != i) { 
+            temp = x[lowestIndex];
+            x[lowestIndex] = x[i];
+            x[i] = temp;
         }
+    }
         return x;
     }
 }
--- src/tests/TestSelectionSort.java
+++ src/tests/TestSelectionSort.java
@@ -4,26 +4,9 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestSelectionSort {
-	
-	@Test
-	public void testPositives() {
-		testPositive();
-	}
-	
-	@Test
-	public void testNegatives() {
-        testNegative();
-	}
-	@Test
-	public void testMixed() {
-        testMixed();
-	}
-	@Test
-	public void testDuplicates() {
-        testDuplicates();
-	}
-    
-    public void testPositive(){
+
+    @Test
+    public void testPositive() {
         int[] arr = new int[5];
         arr[0] = 8;
         arr[1] = 9;
@@ -37,21 +20,18 @@ public class TestSelectionSort {
         Sortedarr[2] = 8;
         Sortedarr[3] = 9;
         Sortedarr[4] = 10;
-        
-        /** add tests to check for this unit test **/
-          
-        assertEquals("The sorted array should be: 2,7,8,9,10", arr, Sortedarr);
-        
+
+        SelectionSort sorter = new SelectionSort();
+        assertArrayEquals("The sorted array should be: 2, 7, 8, 9, 10", Sortedarr, sorter.basicSelectionSort(arr));
     }
-    
-    public void testNegative(){
-        
-        /** Test data contains negative values only **/
+
+    @Test
+    public void testNegative() {
         int[] arr = new int[5];
         arr[0] = -1;
         arr[1] = -5;
         arr[2] = -8;
-        arr[3] = -3;
+        arr[3] = -2;
         arr[4] = -12;
         
         int[] Sortedarr = new int[5];
@@ -60,15 +40,13 @@ public class TestSelectionSort {
         Sortedarr[2] = -5;
         Sortedarr[3] = -2;
         Sortedarr[4] = -1;
-        
-        /** add tests to check for this unit test **/
-          
-        assertEquals("The sorted array should be: -12,-8,-5,-2,-1", arr, Sortedarr);
+
+        SelectionSort sorter = new SelectionSort();
+        assertArrayEquals("The sorted array should be: -12, -8, -5, -2, -1", Sortedarr, sorter.basicSelectionSort(arr));
     }
-    
-    public void testMix(){
-        
-        /** Test data contains with both positive, negative and zeros **/
+
+    @Test
+    public void testMix() {
         int[] arr = new int[5];
         arr[0] = -1;
         arr[1] = 0;
@@ -82,21 +60,19 @@ public class TestSelectionSort {
         Sortedarr[2] = 0;
         Sortedarr[3] = 19;
         Sortedarr[4] = 22;
-        
-        /** add tests to check for this unit test **/
-          
-        assertEquals("The sorted array should be: -12,-1,0,19,22", arr, Sortedarr);
+
+        SelectionSort sorter = new SelectionSort();
+        assertArrayEquals("The sorted array should be: -12, -1, 0, 19, 22", Sortedarr, sorter.basicSelectionSort(arr));
     }
-    
-    public void testDuplicate(){
-        
-        /** Test data contains duplicates **/
+
+    @Test
+    public void testDuplicate() {
         int[] arr = new int[5];
         arr[0] = 44;
         arr[1] = -1;
         arr[2] = -1;
         arr[3] = 9;
-        arr[4] = 1;
+        arr[4] = 44;
         
         int[] Sortedarr = new int[5];
         Sortedarr[0] = -1;
@@ -104,9 +80,8 @@ public class TestSelectionSort {
         Sortedarr[2] = 9;
         Sortedarr[3] = 44;
         Sortedarr[4] = 44;
-        
-        /** add tests to check for this unit test **/
-          
-        assertEquals("The sorted array should be: 1,1,9,44,44", arr, Sortedarr);
+
+        SelectionSort sorter = new SelectionSort();
+        assertArrayEquals("The sorted array should be: -1, -1, 9, 44, 44", Sortedarr, sorter.basicSelectionSort(arr));
     }
 }
